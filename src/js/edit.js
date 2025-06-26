2b07a1974b9dc2817dbbab4e189421fd50e1999e
thiagola92 <thiagola92@gmail.com>
2025-06-25 22:17:32 -0300

    Finish update

--- src/js/edit.js
+++ src/js/edit.js
@@ -1,13 +1,15 @@
+let propertyId = null
+
 async function fillFields(search) {
     let params = new URLSearchParams(search)
-    let id = params.get("id")
+    propertyId = params.get("id")
 
-    if (id == null) {
+    if (propertyId == null) {
         window.location.href = "./error.html"
         return
     }
 
-    let response = await fetch(`http://127.0.0.1:5000/property/${id}`)
+    let response = await fetch(`http://127.0.0.1:5000/property/${propertyId}`)
 
     if (!response.ok) {
         window.location.href = "./error.html"
@@ -18,10 +20,36 @@ async function fillFields(search) {
 
     streetField.value = body["address"]["street"]
     priceField.value = body["price"] / 100
-    planField1.checked = body["plan_id"] == 1 
-    planField2.checked = body["plan_id"] == 2 
-    typeField1.checked = body["type_id"] == 1 
-    typeField2.checked = body["type_id"] == 2 
+    planField1.checked = body["plan_id"] == 1
+    planField2.checked = body["plan_id"] == 2
+    typeField1.checked = body["type_id"] == 1
+    typeField2.checked = body["type_id"] == 2
+}
+
+async function onSubmit(e) {
+    e.preventDefault()
+
+    let response = await fetch("http://127.0.0.1:5000/property", {
+        method: "PUT",
+        headers: {
+            "token": sessionStorage.getItem("token"),
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "id": propertyId,
+            "street": streetField.value,
+            "price": priceField.value,
+            "plan_id": planField1.checked ? 1 : 2,
+            "type_id": typeField1.checked ? 1 : 2,
+        })
+    })
+
+    if (!response.ok) {
+        window.location.href = "./error.html"
+        return []
+    }
+
+    window.location.href = "./list.html"
 }
 
 let previousOnload = window.onload
@@ -29,4 +57,6 @@ let previousOnload = window.onload
 window.onload = async () => {
     previousOnload()
     await fillFields(window.location.search)
+
+    editForm.addEventListener("submit", onSubmit)
 }
